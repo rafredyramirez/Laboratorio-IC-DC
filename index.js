60d9a29777e7110516fb7e56f827ad8e3848cfea
Fredy Ramírez <69448983+rafredyramirez@users.noreply.github.com>
2024-09-09 16:08:25 -0500

    Update index.js
    Pruebas

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 var apiKey = 'Connikey-masaya-VDZLM1Y3';
 var apiToken = 'VDZLM1Y3WTHSNLU3SJNIMLQ2STJPNFU3TJRFMVO4VZDUNK40SDJVNG';
+
+var url = 'https://api.postman.com/collections/24275324-3cf8d09c-1d2a-42fd-9f0d-9f4f3eab6450?access_key='+apiKey;
